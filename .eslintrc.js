cc8928bbc437f432ce25a52a4463abb2cdfa367f
Abshir Ahmed <abshir.ahmed@outlook.com>
2022-10-21 13:49:53 +0100

    :sparkles: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'prefer-arrow-callback': 'warn'
+  }
+};
---
+++ serverless.ts
@@ -0,0 +1,57 @@
+import type { AWS } from '@serverless/typescript';
+
+import schema from "@functions/hello/schema";
+
+const serverlessConfiguration: AWS = {
+  service: 'project-data-hack-team-5',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs16.x',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: {
+    hello: {
+      handler: './src/functions/hello/handler.main',
+      events: [
+        {
+          http: {
+            method: 'post',
+            path: 'hello',
+            request: {
+              schemas: {
+                'application/json': schema,
+              },
+            },
+            cors: true
+          },
+        },
+      ],
+    },
+  },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
